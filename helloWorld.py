cf380e9b30f69423aec8d71b3b2ac1f3b9fe76af
Sarah <sarah.vogetseder.sv@gmail.com>
2025-01-27 19:39:05 +0200

    Added new line for Fixes #2

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,6 @@ print("Git is quite difficult to grasp")
 
 User_input = input("Please can you enter your Name:")
 print(User_input)
+
+User_Explanation = input("Please explain why you think Git is q difficult concept:")
+print(User_Explanation)
